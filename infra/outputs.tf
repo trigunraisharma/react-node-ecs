602a5df9c657c548a3f1a636d9821b35f2bdf713
trigunraisharma <trigunraisharma@gmail.com>
2025-09-19 12:29:41 +0000

    new yml

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,29 +1,29 @@
 resource "aws_ssm_parameter" "s3_bucket" {
-  name  = "/myapp/s3_bucket"
+  name  = "/${var.name}/s3_bucket"
   type  = "String"
   value = aws_s3_bucket.my-react-frontend-app-bucket02.bucket
 }
 
 resource "aws_ssm_parameter" "cloudfront_id" {
-  name  = "/myapp/cloudfront_id"
+  name  = "/${var.name}/cloudfront_id"
   type  = "String"
   value = aws_cloudfront_distribution.s3_distribution.id
 }
 
 resource "aws_ssm_parameter" "ecr_repo_url" {
-  name  = "/myapp/ecr_repo_url"
+  name  = "/${var.name}/ecr_repo_url"
   type  = "String"
   value = aws_ecr_repository.backend.repository_url
 }
 
 resource "aws_ssm_parameter" "ecs_cluster" {
-  name  = "/myapp/ecs_cluster"
+  name  = "/${var.name}/ecs_cluster"
   type  = "String"
-  value = aws_ecs_cluster.my_cluster.name
+  value = aws_ecs_cluster.my-react-node-app-backend.name
 }
 
 resource "aws_ssm_parameter" "ecs_service" {
-  name  = "/myapp/ecs_service"
+  name  = "/${var.name}/ecs_service"
   type  = "String"
-  value = aws_ecs_service.my_service.name
+  value = aws_ecs_service.my-react-node-app-backend-service.name
 }
